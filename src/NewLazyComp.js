c7bb4414140afdc34bfad5a32779bc7ce2646872
Dipesh Sakharkar <dipeshsakharkar1@gmail.com>
2021-04-10 16:28:21 +0530

    first commit

---
+++ src/NewLazyComp.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+class NewLazyComp  extends React.Component{
+
+    render(){
+  return(
+    <div>
+      <h2>I am New Lazy Comp</h2>
+    </div>
+  )
+}
+}
+
+export default NewLazyComp
